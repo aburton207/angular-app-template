c44eb5d7a176b337e5f41a0fc701dec1884bafb4
aburton207 <abu@ihtsdo.org>
2015-06-22 08:25:22 -0300

    WRP-488 adding in dependancies. do not merge....

---
+++ app/shared/controllers/lists.js
@@ -0,0 +1,9 @@
+angular.module('angularAppTemplateApp')
+.controller('ListsController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http){
+  $('#range').on('slide set', function(a,b){
+    if(!$(this).find('.noUi-handle div').length){
+      $(this).find('.noUi-handle').append('<div>'+b+'</div>');
+    }
+    $(this).find('.noUi-handle div').html(b);
+  });
+}]);
---
+++ app/shared/controllers/main.js
@@ -0,0 +1,79 @@
+angular.module('angularAppTemplateApp')
+.controller('MainController',
+  ['$scope', '$animate', 'localStorageService', 'todoService', '$alert', '$timeout',
+  function($scope, $animate, localStorageService, todoService, $alert, $timeout){
+
+  if (typeof(browser_old) == "undefined"){
+    initRipplesWithArrive();
+
+    $(document).arrive('.navbar-toggle', function() {
+      $(this).sideNav({menuWidth: 260, closeOnClick: true});
+    });
+  }
+
+  $scope.theme_colors = [
+    'blue',
+    'light-blue'
+  ];
+
+  // Add todoService to scope
+  service = new todoService($scope);
+  $scope.todosCount = service.count();
+  $scope.$on('todos:count', function(event, count) {
+    $scope.todosCount = count;
+    element = angular.element('#todosCount');
+
+    if ( !element.hasClass('animated') ){
+      $animate.addClass(element, 'animated bounce', function() {
+        $animate.removeClass(element, 'animated bounce');
+      });
+    }
+  });
+
+  $scope.fillinContent = function(){
+    $scope.htmlContent = 'content content';
+  };
+
+  // theme changing
+  $scope.changeColorTheme = function(cls){
+    $scope.theme.color = cls;
+  };
+
+  $scope.changeTemplateTheme = function(cls){
+    $scope.theme.template = cls;
+  };
+
+  if ( !localStorageService.get('theme') ) {
+    theme = {
+      color: 'theme-blue',
+      template: 'theme-template-dark'
+    };
+    localStorageService.set('theme', theme);
+  }
+  localStorageService.bind($scope, 'theme');
+
+  var introductionAlert = $alert({
+    title: 'Welcome to IHTSDO Patterns Library',
+    content: 'This is a ux demo',
+    placement: 'top',
+    type: 'theme',
+    show: false,
+    template: 'assets/tpl/partials/alert-introduction.html',
+    animation: 'mat-grow-top-right'
+  });
+
+  if(!localStorageService.get('alert-introduction')) {
+    $timeout(function(){
+      $scope.showIntroduction();
+      localStorageService.set('alert-introduction', 1);
+    }, 1750);
+  }
+
+  $scope.showIntroduction = function(){
+    introductionAlert.show();
+  };
+
+
+
+
+}]);
